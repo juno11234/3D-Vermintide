d3149fa26d42429d4e43e87949162c8a15d87ea8
juno11234 <cozam2@naver.com>
2025-08-26 13:03:07 +0900

    해쉬셋

--- Assets/2_Scripts/Player/PlayerWepon/GreatSwordSkill.cs
+++ Assets/2_Scripts/Player/PlayerWepon/GreatSwordSkill.cs
@@ -7,29 +7,41 @@ using Random = UnityEngine.Random;
 
 public class GreatSwordSkill : MonoBehaviour
 {
-    int damage = 100;
 
-    [SerializeField]
-    private List<SFXData> hit;
+    [SerializeField] private int damage = 100;
 
+    //사운드
+    [SerializeField] private List<SFXData> hit;
+
+    private HashSet<IFighter> damaged = new HashSet<IFighter>();
+    private void OnEnable()
+    {
+        damaged.Clear();
+    }
     private void OnTriggerEnter(Collider other)
     {
         if (LayerMask.NameToLayer("Enemy") == other.gameObject.layer)
         {
+
             //Debug.Log("공격!");
             int index = Random.Range(0, hit.Count);
+
             SFXManager.Instance.Play(hit[index]);
+
             var monster = CombatSystem.Instance.GetMonsterOrNull(other);
-            if (monster != null)
+            if (monster != null && damaged.Contains(monster) == false)
             {
-                CombatEvents combatEvents = new CombatEvents();
-                combatEvents.Sender = Player.CurrentPlayer;
-                combatEvents.Receiver = monster;
-                combatEvents.Damage = damage;
-                combatEvents.HitPosition = other.ClosestPoint(transform.position);
-                combatEvents.Collider = other;
+                CombatEvents combatEvents = new CombatEvents
+                {
+                    Sender = Player.CurrentPlayer,
+                    Receiver = monster,
+                    Damage = damage,
+                    HitPosition = other.ClosestPoint(transform.position),
+                    Collider = other
+                };
 
                 CombatSystem.Instance.AddInGameEvent(combatEvents);
+                damaged.Add(monster);
             }
         }
     }
